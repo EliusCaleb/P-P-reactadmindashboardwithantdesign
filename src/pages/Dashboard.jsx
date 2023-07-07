eb37cf724739f1651af8ce1075a686a18af13ed5
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-07 15:33:04 +0300

    finshed the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { Typography, Card, Space, Statistic, Table } from "antd";
-import { getOrders } from "../api/data";
+import { getOrders, getRevenue } from "../api/data";
 import {
   ShoppingCartOutlined,
   DollarCircleOutlined,
   ShoppingOutlined,
   UserOutlined,
 } from "@ant-design/icons";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 const Dashboard = () => {
   return (
     <div>
@@ -81,11 +99,13 @@ const Dashboard = () => {
         </Space>
         <Space>
           <RecentOrders />
+          <DashboardChart />
         </Space>
       </Space>
     </div>
   );
 };
+//card on the upperpart of the dashboard
 function DashboardCard({ title, value, icon }) {
   return (
     <Card>
@@ -96,22 +116,24 @@ function DashboardCard({ title, value, icon }) {
     </Card>
   );
 }
-
+//recentorders
 function RecentOrders() {
   const [dataSource, setDataSource] = useState([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
     getOrders().then((res) => {
-      console.log(res.products);
-      setDataSource(res?.products.splice(0,4));
+      console.log(res);
+      setDataSource(res?.products.splice(0, 4));
       setLoading(false);
     });
   }, []);
 
   return (
     <>
-      <Typography.Text style={{fontWeight:'bold'}}>Recent Orders</Typography.Text>
+      <Typography.Text style={{ fontWeight: "bold" }}>
+        Recent Orders
+      </Typography.Text>
       <Table
         columns={[
           {
@@ -129,10 +151,66 @@ function RecentOrders() {
         ]}
         loading={loading}
         dataSource={dataSource}
+        key={dataSource.id}
         pagination={false}
       ></Table>
     </>
   );
 }
+//charts bargraph
 
+function DashboardChart() {
+  const [revenueData, setRevenueData] = useState({
+    labels: [],
+    datasets: [],
+  });
+  useEffect(() => {
+    getRevenue().then((res) => {
+      const labels = res.carts.map((cart) => {
+        return `User ${cart.userId}`;
+      });
+      const data = res.carts.map((cart) => {
+        return cart.discountedTotal;
+      });
+
+      const dataSource = {
+        labels,
+        datasets: [
+          {
+            label: "Renegade",
+            data: data,
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+          },
+          {
+            label: "Compass",
+            data: data,
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+          },
+        ],
+      };
+
+      setRevenueData(dataSource);
+    });
+  }, []);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Jeep Revenue Bar Chart",
+      },
+    },
+  };
+
+  return (
+    <>
+      <Card style={{ width: 600, height: 350, marginLeft: 5, padding: 3 }}>
+        <Bar options={options} data={revenueData} />;
+      </Card>
+    </>
+  );
+}
 export default Dashboard;
