9ab36329b8e50f3288882380a05917b6e3c15ec0
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-07 11:42:22 +0300

    created the footer

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,8 +1,13 @@
+import Typography from 'antd/es/typography/Typography'
 import React from 'react'
 
 const AppFooter= () => {
   return (
-    <div>index</div>
+    <div className='appFooter'>
+      <Typography.Link href='https://elius-portfolio.netlify.app/#contact' target={'_blank'}>+254789785656</Typography.Link>
+      <Typography.Link href='https://elius-portfolio.netlify.app/' target={'_blank'}>Privacy Policy</Typography.Link>
+      <Typography.Link href='https://elius-portfolio.netlify.app/' target={'_blank'}>Terms of Use</Typography.Link>
+    </div>
   )
 }
 
