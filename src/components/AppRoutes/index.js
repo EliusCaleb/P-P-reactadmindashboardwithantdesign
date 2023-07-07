00d56f78b27925821ca6e8b4d58783be1f1da36d
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-07 12:12:56 +0300

    setting up the routes for the sidebar component

--- src/components/AppRoutes/index.js
+++ src/components/AppRoutes/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { BrowserRouter,Routes, Route} from 'react-router-dom'
+import Dashboard from '../../pages/Dashbaord/Dashboard'
 
 const AppRoutes = () => {
   return (
-    <div>AppRoutes</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
