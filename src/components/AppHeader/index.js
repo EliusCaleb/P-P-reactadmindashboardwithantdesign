c1b4a7f2585b350c8ffd4926c1b26b445c252ffe
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-07 11:33:34 +0300

    create the set up for the hero section

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {Image,Typography,Space, Badge} from 'antd'
+import { BellFilled, MailOutlined } from "@ant-design/icons";
 
 const AppHeader = () => {
   return (
-    <div>AppHeader</div>
+    <div className='appHeader'>
+        <Image
+          width={40}
+          src={'https://thumbs.dreamstime.com/b/zoo-concept-driver-man-jeep-car-isolated-illustration-219055248.jpg'}>
+
+        </Image>
+        <Typography.Title>JEEP</Typography.Title>
+        <Space>
+          <Badge count={10} dot>
+          <MailOutlined style={{fontSize:24}}/>
+          </Badge>
+          <Badge count={20}> 
+          <BellFilled style={{fontSize:24}}/>
+
+          </Badge>
+         
+        </Space>
+
+      
+
+
+
+
+
+
+
+
+
+    </div>
   )
 }
 
