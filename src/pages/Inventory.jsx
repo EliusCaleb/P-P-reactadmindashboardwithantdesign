1787cd16abfa2fe8078d16990199158f5b47107c
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-07 16:12:20 +0300

    finish inventory  component

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import { Typography } from 'antd'
+import React, { useState, useEffect } from "react";
+import { Avatar, Rate, Typography, Space, Table } from "antd";
+import { getInventory } from "../api/data";
 
 const Inventory = () => {
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getInventory().then((res) => {
+      console.log(res);
+      setDataSource(res?.products);
+      setLoading(false);
+    });
+  }, []);
   return (
     <div>
+      <Space size={20} direction="vertical">
         <Typography.Title level={4}>Inventory </Typography.Title>
+        <Table
+          columns={[
+            {
+              title: "Thumbnail",
+              dataIndex: "thumbnail",
+              render: (link) => {
+                return <Avatar src={link} />;
+              },
+            },
+            {
+              title: "Title",
+              dataIndex: "title",
+            },
+            {
+              title: "Price",
+              dataIndex: "price",
+              render: (value) => <span>${value}</span>,
+            },
+            {
+              title: "Rating",
+              dataIndex: "rating",
+              render: (rating) => {
+                return <Rate value={rating} allowHalf disabled />;
+              },
+            },
+            {
+              title: "Stock",
+              dataIndex: "stock",
+            },
+            {
+              title: "Brand",
+              dataIndex: "brand",
+            },
+            {
+              title: "Category",
+              dataIndex: "category",
+            },
+          ]}
+          loading={loading}
+          dataSource={dataSource}
+          pagination={{
+            pageSize:6
+          }}
+          key={dataSource.id}
+        ></Table>
+      </Space>
     </div>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
