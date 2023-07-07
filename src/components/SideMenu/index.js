75e1018602aa4244a91c1d65f226dca7a6e2e613
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-07 12:00:35 +0300

    designing the sidebar  of the app

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import {Menu} from 'antd'
+import {AppstoreOutlined,ShoppingCartOutlined,ShopOutlined,UserOutlined} from '@ant-design/icons'
 
 const SideMenu = () => {
   return (
-    <div>SideMenu</div>
+    <div className='sideMenu'>
+      <Menu
+         items={
+          [
+            {
+              label: 'Dashboard',
+              icon:<AppstoreOutlined/>,
+              key:'/'
+            },
+            {
+              label: 'Inventory',
+              key:'/inventory',
+              icon:<ShopOutlined/>
+            },
+            {
+              label: 'Orders',
+              key:'/orders',
+              icon:<ShoppingCartOutlined/>
+            },
+            {
+              label: 'Customers',
+              key:'/customers',
+              icon:<UserOutlined/>
+            }
+           
+          ]
+
+         } 
+         
+         
+         >
+
+      </Menu>
+      
+
+
+
+
+
+
+
+    </div>
   )
 }
 
