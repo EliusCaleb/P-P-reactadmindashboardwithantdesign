74dba6aeef87b59b2aae1a0e7970863c3877cf80
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-08 13:08:15 +0300

    create functionality to the Mail and bell  icons

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import {Image,Typography,Space, Badge} from 'antd'
+import React,{useEffect,useState} from 'react';
+import {Image,Typography,Space, Badge,List, Drawer} from 'antd'
 import { BellFilled, MailOutlined } from "@ant-design/icons";
+import { getComments,getOrders } from '../../api/data';
 
 const AppHeader = () => {
+  const [comments,setComments] = useState([])
+  const [orders,setOrders] = useState([])
+  const [commentsOpen, setCommentsOpen]= useState(false)
+  const [notificationOpen, setNotificationOpen]= useState(false)
+  useEffect(()=>{
+    
+     getComments().then(
+        (res) =>{
+            setComments(res.comments.splice(0, 13))
+        }
+    )  
+    getOrders().then(
+      (res) =>{
+          setOrders(res.products)
+      }
+  )    
+},[])
   return (
     <div className='appHeader'>
         <Image
@@ -12,24 +30,49 @@ const AppHeader = () => {
         </Image>
         <Typography.Title>PHONES & PERFUMES</Typography.Title>
         <Space>
-          <Badge count={10} dot>
-          <MailOutlined style={{fontSize:24}}/>
+          <Badge count={comments.length} dot>
+          <MailOutlined style={{fontSize:24}} onClick={()=> setCommentsOpen(true)}/>
           </Badge>
-          <Badge count={20}> 
-          <BellFilled style={{fontSize:24}}/>
+          <Badge count={orders.length}> 
+          <BellFilled style={{fontSize:24}} onClick={()=> setNotificationOpen(true)}/>
 
           </Badge>
          
         </Space>
+        <Drawer title='Comments' open={commentsOpen}
+        onClose={() => {
+          setCommentsOpen(false);
+        }}
+        maskClosable>
+          
+          <List
+          dataSource={comments}
+          renderItem={(item) => {
+            return <List.Item>{item.body}</List.Item>;
+          }}
+        ></List>
 
-      
+          
+        </Drawer>
 
-
-
-
-
-
-
+       <Drawer title='Notification' open={notificationOpen}
+        onClose={() => {
+          setNotificationOpen(false);
+        }}
+        maskClosable>
+          <List
+          dataSource={orders}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Typography.Text strong>{item.title}</Typography.Text> has been
+                ordered!
+              </List.Item>
+            );
+        
+          }}
+        ></List>
+        </Drawer>
 
 
     </div>
