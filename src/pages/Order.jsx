f8dce036c0f0f941fa088840a5795161e78be09a
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-08 11:41:22 +0300

    finish the customer components for the app

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
-import { Typography } from 'antd'
+import React, { useState, useEffect } from "react";
+import {  Typography, Space, Table } from "antd";
+import { getOrders } from "../api/data";
 
 const Order = () => {
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getOrders().then((res) => {
+      console.log(res);
+      setDataSource(res?.products);
+      setLoading(false);
+    });
+  }, []);
   return (
     <div>
+      <Space size={20} direction="vertical">
         <Typography.Title level={4}>Orders </Typography.Title>
-    </div>
-  )
-}
+        <Table
+          columns={[
+            
+            {
+              title: "Title",
+              dataIndex: "title",
+            },
+            {
+              title: "Price",
+              dataIndex: "price",
+              render: (value) => <span>${value}</span>,
+            },
+            {
+              title: "DiscountedPrice",
+              dataIndex: "discountedPrice",
+              render: (value) => <span>${value}</span>,
 
-export default Order
+              
+            },
+            {
+              title: "Quantity",
+              dataIndex: "quantity",
+            },
+            {
+              title: "Total",
+              dataIndex: "total",
+            },
+          ]}
+          loading={loading}
+          dataSource={dataSource}
+          pagination={false}
+          key={dataSource.id}
+        ></Table>
+      </Space>
+    </div>
+  );
+};
+
+export default Order;
